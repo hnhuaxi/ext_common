b226e51cfa44aa388be30bfc281748a44e53d717
Hysios Hu <hysios@gmail.com>
2025-04-22 18:30:05 +0800

    Fixed the insecure.go file

--- insecure/insecure.go
+++ insecure/insecure.go
@@ -4,14 +4,15 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"log"
+	"os"
 
 	_ "embed"
 )
 
-//go:embed platform.crt
+// //go:embed platform.crt
 var certPEM []byte
 
-//go:embed platform.key
+// //go:embed platform.key
 var keyPEM []byte
 
 var (
@@ -23,6 +24,16 @@ var (
 
 func init() {
 	var err error
+	if len(certPEM) == 0 || len(keyPEM) == 0 {
+		certPEM, err = loadFileFromFile(os.Getenv("PLATFORM_CERT_FILE"))
+		if err != nil {
+			log.Fatalln("Failed to load certificate:", err)
+		}
+		keyPEM, err = loadFileFromFile(os.Getenv("PLATFORM_KEY_FILE"))
+		if err != nil {
+			log.Fatalln("Failed to load key:", err)
+		}
+	}
 	Cert, err = tls.X509KeyPair([]byte(certPEM), []byte(keyPEM))
 	if err != nil {
 		log.Fatalln("Failed to parse key pair:", err)
@@ -34,4 +45,12 @@ func init() {
 
 	CertPool = x509.NewCertPool()
 	CertPool.AddCert(Cert.Leaf)
+}
+
+func loadFileFromFile(path string) ([]byte, error) {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	return content, nil
 }
